4ac1ad3b5fa3798ff978ec67f981be515d79c66b
calcifer chang <c@lcifer.com>
2025-10-15 00:02:35 -0400

    first text iteration

--- sketch.js
+++ sketch.js
@@ -5,13 +5,31 @@ function setup() {
 }
 
 function mouseClicked() {
+  switch (state) {
+    case "text_hover":
+      // add class to the highest level element?
+      // as long as the cursor is over an element, the highest level element will have a class applied to it, which will allow actions to be applied to it.
+      break;
+    default:
+      addText();
+      break;
+  }
+}
+
+function addText() {
   stickyNote = createDiv('Note');
-  stickyNote.position(5, 5);
+  stickyNote.position(mouseX, mouseY);
   stickyNote.size(80, 20);
   stickyNote.style('font-size', '16px');
   stickyNote.style('font-family', 'Comic Sans MS');
   stickyNote.style('padding', '5px');
   stickyNote.draggable();
-  stickyNote.mouseClicked()
+  stickyNote.elt.addEventListener('click', (event) => {
+    event.stopPropagation();
+    console.log('Button clicked');
+  });
+  // Remember which note was clicked (this text)
+  // Key inputs now will affect this text (how do select an element to have its innerHTML changed?)
 }
 
+let state = ""
